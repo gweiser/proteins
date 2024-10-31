7200a55c60654c47303c5c1ea7d129fbb3707a90
gregolator <gweiser54@icloud.com>
2024-10-31 16:52:58 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from textwrap import wrap
+def convert(dna_sequence):
+    """Convert DNA sequence to RNA equivalent"""
+    rna_sequence = []
+    for i in dna_sequence:
+        # Find counterpart for each organic base in DNA
+        match i:
+            case "A":
+                rna_sequence.append("U")
+            case "T":
+                rna_sequence.append("A")
+            case "G":
+                rna_sequence.append("C")
+            case "C":
+                rna_sequence.append("G")
+
+    return "".join(rna_sequence)
+
+
+def sort_triplets(rna_sequence):
+    """Sort triplets to start and end properly"""
+    # Split RNA into triplets
+    triplets_unsorted = wrap(rna_sequence, 3)
+
+    
+    start_triplet = "AUG"
+    end_triplets = ["UAA", "UAG", "UGA"]
+
+    # Find indices for start and end of useable RNA 
+    start_index = triplets_unsorted.index(start_triplet)
+    for i in triplets_unsorted:
+        if i in end_triplets:
+            end_index = triplets_unsorted.index(i)
+            break
+    # Cut out unnecessary organic bases from RNA
+    triplets_sorted = triplets_unsorted[start_index+1:end_index]
+
+    return triplets_sorted
+
+
+
+
+
+def main():
+    dna_sequence = "TACGGGTTTCCCATTGCT"
+
+    print(sort_triplets(convert(dna_sequence)))
+
+
+
+main()
